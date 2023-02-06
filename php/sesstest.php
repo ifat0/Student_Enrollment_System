343f2998452f4598e6db3b74b2e71514c61eac65
ifat0 <ifat.tafhim1@gmail.com>
2023-02-06 21:57:30 +0600

    SMS

---
+++ php/sesstest.php
@@ -0,0 +1,118 @@
+<?php
+    session_start();
+    if (isset($_SESSION['id'])){
+    include "dbh.class.php";
+    $data=new dbh();
+    $sql=$data->getdata("SELECT COUNT(id) from student; ");
+    $sql1=$data->getdata("SELECT COUNT(fid) from faculty; ");
+    $sql2=$data->getdata("SELECT COUNT(hdid) from hod; ");
+    $sql3=$data->getdata("SELECT COUNT(smid) from schoolmanager; ");
+    $sql4=$data->getdata("SELECT COUNT(uaid) from universityauthority; ");
+    $row=$sql->fetch(PDO::FETCH_ASSOC);
+    $row1=$sql1->fetch(PDO::FETCH_ASSOC);
+    $row2=$sql2->fetch(PDO::FETCH_ASSOC);
+    $row3=$sql3->fetch(PDO::FETCH_ASSOC);
+    $row4=$sql4->fetch(PDO::FETCH_ASSOC);
+    
+?>
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="../css/admin.css">
+
+</head>
+
+<body>
+    <header>
+        <span>
+            Admin Dashboard
+        </span>
+        <a class="log" href="registeredcourse.php"> See Selected Courses</a>
+        <a class="log" href="logout.php"><button>Log Out</button></a>
+    </header>
+
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-lg-2">
+                <div class="sidebar">
+                    <a href="studentreg.php">+ Add Student</a>
+                    <a href="facultyreg.php">+ Add Faculty</a>
+                    <a href="#">+ Add School Manager</a>
+                    <a href="#">+ Add HOD</a>
+                    <a href="uareg.php">+Add Univeristy Authority</a>
+                </div>
+            </div>
+            <div class="col-lg-10">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="student">
+                            <p>Total HOD</p>
+
+                            <span><?php echo $row2['COUNT(hdid)']; ?></span>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-6">
+                        <div class="student">
+                            <p>Total Students</p>
+                            <span><?php echo $row['COUNT(id)']; ?></span>
+                        </div>
+                    </div>
+                    <div class="col-lg-6">
+                        <div class="student">
+                            <p>Total Faculties</p>
+                            <span><?php echo $row1['COUNT(fid)']; ?>
+                            </span>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-6">
+                        <div class="student">
+                            <p>Total School Managers</p>
+                            <span><?php echo $row3['COUNT(smid)']; ?></span>
+                        </div>
+                    </div>
+                    <div class="col-lg-6">
+                        <div class="student">
+                            <p>Total University Authorities</p>
+                            <span><?php echo $row4['COUNT(uaid)']; ?>
+                            </span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
+    </script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+    -->
+</body>
+
+</html>
+<?php 
+}else{
+    echo '<script>alert(" Hello NOOB hacker!");</script>';
+}
+
+?>
---
+++ php/uareg.php
@@ -0,0 +1,133 @@
+<?php
+include_once 'dbh.class.php';
+// if(isset($_POST['submit'])) header("location:login.class.php");
+// else{
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Authority Registration</title>
+
+    <!-- Font Icon -->
+    <link rel="stylesheet" href="../fonts/material-icon/css/material-design-iconic-font.min.css">
+
+    <!-- Main css -->
+    <link rel="stylesheet" href="../css/reg_style.css">
+</head>
+
+<body>
+
+    <div class="main">
+
+        <!-- Sign up form -->
+        <section class="signup">
+            <div class="container">
+                <div class="signup-content">
+                    <div class="signup-form">
+                        <h3 class="form-title">University Authority Registration</h3>
+                        <form action="uareg.php" method="POST" class="register-form" id="register-form">
+                            <div class="form-group">
+                                <label for="name"><i class="zmdi zmdi-account material-icons-name"></i></label>
+                                <input type="number" name="sid" id="sid" placeholder="Enter Authority ID" />
+                            </div>
+                            <div class="form-group">
+                                <label for="name"><i class="zmdi zmdi-account material-icons-name"></i></label>
+                                <input type="text" name="name" id="name" placeholder="Enter Authority's Name" />
+                            </div>
+                            <div class="form-group">
+                                <label for="email"><i class="zmdi zmdi-email"></i></label>
+                                <input type="email" name="email" id="email" placeholder="Enter Email" />
+                            </div>
+                            <div class="form-group">
+                                <label for="pass"><i class="zmdi zmdi-lock"></i></label>
+                                <input type="password" name="pass" id="pass" placeholder="Password" />
+                            </div>
+                            <div class="form-group">
+                                <label for="re-pass"><i class="zmdi zmdi-lock-outline"></i></label>
+                                <input type="password" name="re_pass" id="re_pass" placeholder="Repeat password" />
+                            </div>
+                            <div class="form-group">
+                                <label for="phone"><i class="zmdi zmdi-lock"></i></label>
+                                <input type="number" name="phone" id="num" placeholder="Phone Number" />
+                            </div>
+                            <div class="form-group">
+                                <label for="Address"><i class="zmdi zmdi-email"></i></label>
+                                <input type="text" name="address" id="address" placeholder="Enter Address" />
+                            </div>
+                            <div class="form-group">
+                                <input type="checkbox" name="agree-term" id="agree-term" class="agree-term" />
+                                <label for="agree-term" class="label-agree-term"><span><span></span></span>I agree all
+                                    statements in <a href="terms.class.php" class="term-service">Terms of
+                                        service</a></label>
+                            </div>
+                            <div class="form-group form-button">
+                                <input type="submit" name="signup" id="signup" class="form-submit" value="Register" />
+                            </div>
+                        </form>
+                    </div>
+                    <div class="signup-image">
+                        <figure><img src="../images/signup-image.jpg" alt="sign up image"></figure>
+                        <a href="admin.php" class="signup-image-link">
+                            <h3>Admin Dashboard</h3>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+
+    <!-- JS -->
+    <script src="../vendor/jquery/jquery.min.js"></script>
+    <script src="../js/main.js"></script>
+</body>
+
+</html>
+
+<?php
+// }
+class Registration extends Dbh{
+    public $sid;
+    public $name;
+    public $phone;
+    public $address;
+    public $email;
+    public $password;
+}
+
+$data=new Registration();
+//below 5 lines are written if a email is already exist or not and later we used a condition to check that.
+if(isset($_POST['email'])){
+    $sql=$data->getData("select email from student where email='".$_POST['email']."' ");
+    $rows=$sql->fetch(PDO::FETCH_ASSOC);
+}
+
+//Generating a random string for email verification
+if (isset($_POST['sid']) && isset($_POST['name']) && isset($_POST['email'])&&isset($_POST['phone'])&&isset($_POST['address'])){
+
+//If email is not registered yet
+    if($rows==null){
+
+        //checking if the password and confirm password are same
+        if($_POST['pass']==$_POST['re_pass']){
+
+            // if (!empty($_POST['name']) && !empty( $_POST['phone']) && !empty($_POST['address']) && !empty($_POST['email']) && !empty($_POST['pass'])){
+                $database=new Dbh();
+
+                //Inserting Data into Users table
+                $database->connect($sql="Insert into universityauthority(ua_Id, ua_Name, ua_Email, ua_Phone, ua_address, ua_pass) values('".$_POST['sid']."','".$_POST['name']."','".$_POST['email']."','".$_POST['phone']."','".$_POST['address']."','".$_POST['pass']."')");
+                
+            // }
+        }
+        else{
+            echo '<script>alert("Password not matched")</script>';
+        }
+    }
+
+}
+
+?>
